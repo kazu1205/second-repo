13a0786c0b13cfc17241a3f06336527bb45d0ce9
Kazuaki Okumura <Kazu@Kazuakis-MacBook-Air.local>
2013-04-09 21:07:12 -0400

    initial files

---
+++ js/app.js
@@ -0,0 +1,9 @@
+$(function(event){
+
+	$("#startSwapping").click(function(event){
+		$("body").css("background-image", "url(\"images/bg1.jpg\")");
+		$("h2").html("Peanut Butter Cookie Evangelist");
+	});
+
+});
+
